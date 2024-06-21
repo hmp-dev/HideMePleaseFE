c0b80c31ccf0330eb126e261015175455e5dc211
Javeed Ishaq <ishaqjaveed1@gmail.com>
2024-06-21 06:21:04 +0500

    notification api integration and qas

---
+++ lib/features/settings/infrastructure/dtos/notification_dto.dart
@@ -0,0 +1,52 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+import 'package:mobile/app/core/cubit/cubit.dart';
+import 'package:mobile/features/settings/domain/entities/notification_entity.dart';
+
+part 'notification_dto.g.dart';
+
+@JsonSerializable()
+class NotificationDto extends Equatable {
+  final String? id;
+  final String? createdAt;
+  final String? title;
+  final String? body;
+  final String? type;
+  final String? params;
+
+  const NotificationDto({
+    this.id,
+    this.createdAt,
+    this.title,
+    this.body,
+    this.type,
+    this.params,
+  });
+
+  factory NotificationDto.fromJson(Map<String, dynamic> json) =>
+      _$NotificationDtoFromJson(json);
+
+  Map<String, dynamic> toJson() => _$NotificationDtoToJson(this);
+
+  @override
+  List<Object?> get props {
+    return [
+      id,
+      createdAt,
+      title,
+      body,
+      type,
+      params,
+    ];
+  }
+
+  NotificationEntity toEntity() => NotificationEntity(
+        id: id!,
+        createdAt: createdAt ?? '',
+        title: title ?? '',
+        body: body ?? '',
+        type: type ?? '',
+        params: params ?? '',
+      );
+}
