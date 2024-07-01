81b9cdff71c89e8b5528143393cf20350b622dee
Javeed Ishaq <ishaqjaveed1@gmail.com>
2024-07-01 18:28:16 +0500

    privacy polciy and tos pages content added

--- lib/features/settings/presentation/screens/privacy_policy_screen.dart
+++ lib/features/settings/presentation/screens/privacy_policy_screen.dart
@@ -4,8 +4,10 @@ import 'package:mobile/app/theme/theme.dart';
 import 'package:mobile/features/common/presentation/views/base_scaffold.dart';
 import 'package:mobile/generated/locale_keys.g.dart';
 
-class PrivacyPolicyScreen extends StatelessWidget {
-  const PrivacyPolicyScreen({super.key});
+class PrivacyPolicyScreen extends StatefulWidget {
+  const PrivacyPolicyScreen({
+    super.key,
+  });
 
   static push(BuildContext context) async {
     return await Navigator.push(
@@ -17,6 +19,160 @@ class PrivacyPolicyScreen extends StatelessWidget {
   }
 
   @override
+  State<PrivacyPolicyScreen> createState() => _PrivacyPolicyScreenState();
+}
+
+class _PrivacyPolicyScreenState extends State<PrivacyPolicyScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  final String terms = '''
+개인정보처리방침
+
+주식회사 하이드미플리즈(이하 ‘회사’라 합니다)는 개인정보보호법 및 정보통신망 이용촉진 및 정보보호 등에 관한 법률에 따라 회사가 운영하는 Hide Me, Please “애플리케이션”(이하 “애플리케이션”을 “APP”이라고 합니다) 이용자의 개인정보 및 권익을 보호하고 개인정보와 관련한 이용자의 고충을 원활하게 처리할 수 있도록 다음과 같은 처리방침을 두고 있습니다.
+
+회사는 개인정보처리방침의 지속적인 개선을 위하여 개인정보처리방침을 개정하는데 필요한 절차를 정하고 있습니다. 그리고 본 처리방침은 수시로 내용이 변경될 수 있으니 정기적으로 방문하여 확인하시기 바랍니다.
+
+제1조(개인정보의 수집 및 이용목적)
+
+① 개인정보 수집 및 이용목적
+
+본인확인에 따른 서비스 부정이용 방지, 각종고지 · 통지, 고충처리, 분쟁조정을 위한 기록 보존 등을 목적으로 개인정보를 처리합니다.
+앱 내 서비스 제공 및 본인인증 등을 위한 목적으로 개인정보를 처리합니다.
+기타 회사의 정상적인 영업에 관계된 행위의 목적으로 개인정보를 처리합니다.
+
+② 수집 및 이용하는 개인정보 항목
+
+본인인증: 성명, 생년월일, 연락처
+
+③ 수집방법
+
+“APP”에 마련된 개인정보 입력란에 본인이 직접 입력하는 방식
+
+실명확인을 위하여 마련된 대체수단에 직접 입력하는 방식
+생성정보 수집 툴을 이용한 “APP”이용 로그 기록 자동수집
+
+제2조(개인정보의 보유 및 이용기간)
+
+① 회사는 법령에 따른 개인정보 보유·이용기간 또는 정보주체로부터 개인정보를 수집시에 동의 받은 개인정보 보유,이용 기간 내에서 개인정보를 처리·보유합니다.
+
+② 개인정보 처리 및 보유 기간은 다음과 같습니다.
+
+보유 및 이용기간 : 회원 탈회 또는 개인정보의 수집·이용 등에 대한 동의를 철회할 때까지
+기타 법률에 의해 이용자의 개인정보를 보존해야 할 필요가 있는 경우에는 해당 법률의 규정에 따릅니다.
+정보통신망 이용촉진 및 정보보호에 등에 관한 법률에 따라 “APP”를 1년간 로그인하지 아니하는 회원의 개인정보를 보호하기 위하여 개인정보 분리보관 또는 파기 등 필요한 조치를 취합니다.
+
+제3조(정보주체 및 법정대리인의 권리와 그 행사방법)
+
+① 정보주체는 회사에 대해 언제든지 개인정보 열람,정정,삭제,처리정지 요구 등의 권리를 행사할 수 있습니다.
+
+② 제1항에 따른 권리 행사는 회사에 대해 개인정보보호법 시행령 제41조제1항에 따라 서면, 전자우편, 모사전송(FAX) 등을 통하여 하실 수 있으며 회사는 이에 대해 지체 없이 조치하겠습니다.
+
+③ 제1항에 따른 권리 행사는 정보주체의 법정대리인이나 위임을 받은 자 등 대리인을 통하여 하실 수 있습니다. 이 경우 개인정보 보호법 시행규칙 별지 제11호 서식에 따른 위임장을 제출하셔야 합니다.
+
+④ 개인정보 열람 및 처리정지 요구는 개인정보보호법 제35조 제5항, 제37조 제2항에 의하여 정보주체의 권리가 제한 될 수 있습니다.
+
+⑤ 개인정보의 정정 및 삭제 요구는 다른 법령에서 그 개인정보가 수집 대상으로 명시되어 있는 경우에는 그 삭제를 요구할 수 없습니다.
+
+⑥ 회사는 정보주체 권리에 따른 열람의 요구, 정정·삭제의 요구, 처리정지의 요구 시 열람 등 요구를 한 자가 본인이거나 정당한 대리인인지를 확인합니다.
+
+제4조 (개인정보 자동 수집 장치의 설치·운영 및 거부에 관한 사항)
+
+① 회사는 개별적인 맞춤서비스를 제공하기 위해 이용정보를 저장하고 수시로 불러오는 ‘쿠키(cookie)’를 사용합니다.
+
+② 쿠키는 웹사이트를 운영하는데 이용되는 서버(http)가 이용자의 컴퓨터 브라우저에게 보내는 소량의 정보이며 이용자들의 PC 컴퓨터내의 하드디스크에 저장되기도 합니다.
+
+쿠키의 사용 목적 : 이용자가 방문한 각 서비스와 웹 사이트들에 대한 방문 및 이용형태, 인기 검색어, 보안접속 여부, 등을 파악하여 이용자에게 최적화된 정보 제공 및 타겟 마케팅을 위해 사용됩니다.
+쿠키의 설치․운영 및 거부 : 웹브라우저 상단의 도구>인터넷 옵션>개인정보 메뉴의 옵션 설정을 통해 쿠키 저장을 거부 할 수 있습니다. 다. 쿠키 저장을 거부할 경우 맞춤형 서비스 이용에 어려움이 발생할 수 있습니다.
+
+제5조(개인정보 파기절차 및 방법)
+
+① 회사은 원칙적으로 개인정보 보유기간의 경과, 처리목적 달성 등 그 개인정보가 불필요하게 되었을 때에는 지체없이 파기합니다. 다만, 다른 법령에 따라 보존하여야 하는 경우에는 그러하지 않을 수 있습니다. 파기의 절차, 기한 및 방법은 다음과 같습니다.
+
+파기절차
+
+불필요한 개인정보 및 개인정보파일은 개인정보 보호책임자의 책임 하에 관련법령 및 내부방침 절차에 따라 안전하게 파기합니다.
+
+파기기한
+
+개인정보는 개인정보의 보유기간이 경과된 경우 정당한 사유가 없는 한 보유기간의 종료일로부터 5일이내에, 개인정보의 처리 목적 달성 등 그 개인정보가 불필요하게 되었을 때에는 정당한 사유가 없는 한 개인정보의 처리가 불필요한 것으로 인정되는 날로부터 5일 이내에 그 개인정보를 파기합니다.
+
+파기방법
+
+가.전자적 형태의 정보는 기록을 재생할 수 없는 기술적 방법을 사용합니다.
+나.종이에 출력된 개인정보는 분쇄기로 분쇄하거나 소각을 통하여 파기합니다.
+
+제6조(개인정보의 안전성 확보 조치)
+
+회사는 개인정보보호법 제29조에 따라 다음과 같이 안전성 확보에 필요한 기술적/관리적 및 물리적 조치를 하고 있습니다.
+
+정기적인 자체 감사 실시
+
+개인정보 취급 관련 안정성 확보를 위해 정기적(분기 1회)으로 자체 감사를 실시하고 있습니다.
+
+개인정보 취급 직원의 최소화 및 교육
+
+개인정보를 취급하는 직원을 지정하고 담당자에 한정시켜 최소화 하여 개인정보를 관리하는 대책을 시행하고 있습니다.
+
+내부관리계획의 수립 및 시행
+
+개인정보의 안전한 처리를 위하여 내부관리계획을 수립하고 시행하고 있습니다.
+
+해킹 등에 대비한 기술적 대책
+
+회사는 해킹이나 컴퓨터 바이러스 등에 의한 개인정보 유출 및 훼손을 막기 위하여 보안프로그램을 설치하고 주기적인 갱신·점검을 하며 외부로부터 접근이 통제된 구역에 시스템을 설치하고 기술적/물리적으로 감시 및 차단하고 있습니다.
+
+개인정보의 암호화
+
+이용자의 개인정보는 비밀번호는 암호화 되어 저장 및 관리되고 있어, 본인만이 알 수 있으며 중요한 데이터는 파일 및 전송 데이터를 암호화 하거나 파일 잠금 기능을 사용하는 등의 별도 보안기능을 사용하고 있습니다.
+
+접속기록의 보관 및 위변조 방지
+
+개인정보처리시스템에 접속한 기록을 최소 6개월 이상 보관, 관리하고 있으며, 접속 기록이 위변조 및 도난, 분실되지 않도록 보안기능 사용하고 있습니다.
+
+개인정보에 대한 접근 제한
+
+개인정보를 처리하는 데이터베이스시스템에 대한 접근권한의 부여,변경,말소를 통하여 개인정보에 대한 접근통제를 위하여 필요한 조치를 하고 있으며 침입차단시스템을 이용하여 외부로부터의 무단 접근을 통제하고 있습니다.
+
+제7조(정보주체의 권익침해에 대한 구제방법)
+
+아래의 기관은 회사와는 별개의 기관으로서, 회사의 자체적인 개인정보 불만처리, 피해구제 결과에 만족하지 못하시거나 보다 자세한 도움이 필요하시면 문의하여 주시기 바랍니다.
+
+개인정보 침해신고센터 (한국인터넷진흥원 운영)
+소관업무 : 개인정보 침해사실 신고, 상담 신청
+홈페이지 : ​http://privacy.kisa.or.kr/
+전화 : (국번없이) 118
+주소 : (58324) 전남 나주시 진흥길 9(빛가람동 301-2) 3층 개인정보침해신고센터
+
+개인정보 분쟁조정위원회
+소관업무 : 개인정보 분쟁조정신청, 집단분쟁조정 (민사적 해결)
+홈페이지 : ​http://www.kopico.go.kr/
+전화 : (국번없이) 1833-6972
+주소 : (03171)서울특별시 종로구 세종대로 209 정부서울청사 4층
+
+대검찰청 사이버범죄수사단 : 02-3480-3573 (http://www.spo.go.kr/)
+경찰청 사이버안전국 : 182 (http://cyberbureau.police.go.kr/)
+
+제8조(개인정보 보호책임자)
+
+① 회사는 개인정보 처리에 관한 업무를 총괄해서 책임지고, 개인정보 처리와 관련한 정보주체의 불만처리 및 피해구제 등을 위하여 아래와 같이 개인정보 보호책임자를 지정하고 있습니다.
+
+개인정보 보호책임자
+
+준법감시인 김대성
+연락처: ds.kim@hidemeplease.xyz
+
+② 정보주체께서는 회사의 서비스를 이용하시면서 발생한 모든 개인정보 보호 관련 문의, 불만처리, 피해구제 등에 관한 사항을 개인정보 보호책임자께 문의하실 수 있습니다. 회사는 정보주체의 문의에 대해 지체 없이 답변 및 처리해드릴 것입니다.
+
+제9조(개인정보 처리방침 변경)
+
+이 개인정보처리방침은 2019.05.01.부터 적용되며, 법령 및 방침에 따른 변경내용의 추가, 삭제 및 정정이 있는 경우에는 변경사항의 시행 7일 전부터 공지사항을 통하여 고지할 것입니다.
+
+''';
+
+  @override
   Widget build(BuildContext context) {
     return BaseScaffold(
       title: LocaleKeys.privacyPolicyTitle.tr(),
@@ -24,100 +180,24 @@ class PrivacyPolicyScreen extends StatelessWidget {
       onBack: () {
         Navigator.pop(context);
       },
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: ListView(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              buildParagraph(
-                  '주식회사 하이드미플리즈(이하 "회사"라 합니다)는 개인정보보호법 및 정보통신망 이용촉진 및 정보 보호 등에 관한 법률에 따라 회사가 운영하는 Hide Me, Please "애플리케이션"(이하 "애플리케이션"을 "APP"이라고 합니다) 이용자의 개인정보 및 권익을 보호하고 개인정보와 관련한 이용자의 고충을 원활하게 처리할 수 있도록 다음과 같은 처리 방침을 두고 있습니다.'),
-              buildParagraph(
-                  '회사는 개인정보처리방침의 지속적인 개선을 위하여 개인정보처리방침을 개정하는데 필요한 절차를 정하고 있습니다. 그리고 본 처리방침은 수시로 내용이 변경될 수 있으니 정기적으로 방문하여 확인 하시기 바랍니다.'),
-              buildSubParagraph('제1조(개인정보의 수집 및 이용목적)'),
-              buildSubParagraph('① 개인정보 수집 및 이용목적'),
-              buildOrderedListItem(
-                  '1. 본인확인에 따른 서비스 부정이용 방지, 각종 고지ㆍ통지, 고충처리, 분쟁조정을 위한 기록 보존 등을 목적으로 개인정보를 처리합니다.'),
-              buildOrderedListItem(
-                  '2. 앱 내 서비스 제공 및 본인인증 등을 위한 목적으로 개인정보를 처리합니다.'),
-              buildOrderedListItem(
-                  '3. 기타 회사의 정상적인 영업에 관계된 행위의 목적으로 개인정보를 처리합니다.'),
-              buildSubParagraph('② 수집 및 이용하는 개인정보 항목'),
-              buildOrderedListItem('1. 본인인증: 성명, 생년월일, 연락처'),
-              buildSubParagraph('③ 수집방법'),
-              buildOrderedListItem('1. "APP"에 마련된 개인정보 입력란에 본인이 직접 입력하는 방식'),
-              buildOrderedSubListItem('1.1. 실명확인을 위하여 마련된 대체수단에 직접 입력하는 방식'),
-              buildOrderedSubListItem(
-                  '1.2. 생성정보 수집 툴을 이용한 "APP" 이용 로그 기록 자동수집'),
-              buildSubParagraph('제2조(개인정보의 보유 및 이용기간)'),
-              buildOrderedListItem(
-                  '① 회사는 법령에 따른 개인정보 보유 이용기간 또는 정보주체로부터 개인정보를 수집시에 동의 받은 개인정보 보유, 이용 기간 내에서 개인정보를 처리• 보유합니다.'),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Text(
+                  terms,
+                  textAlign: TextAlign.start,
+                  style: fontBodySm(color: fore2),
+                ),
+              ),
             ],
           ),
         ),
-      ),
-    );
-  }
-
-  Widget buildParagraph(String text) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Text(
-        text,
-        style: fontBodySm(color: fore2),
-      ),
-    );
-  }
-
-  Widget buildSubParagraph(String text) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Text(
-        text,
-        style: fontBodySm(color: fore2),
-      ),
-    );
-  }
-
-  Widget buildOrderedListItem(String text) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 8.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            '${text.split(' ')[0]} ',
-            style: fontBodySm(color: fore2),
-          ),
-          Expanded(
-            child: Text(
-              text.substring(text.indexOf(' ') + 1),
-              style: fontBodySm(color: fore2),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget buildOrderedSubListItem(String text) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 16.0, bottom: 8.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            '${text.split(' ')[0]} ',
-            style: fontBodySm(color: fore2),
-          ),
-          Expanded(
-            child: Text(
-              text.substring(text.indexOf(' ') + 1),
-              style: fontBodySm(color: fore2),
-            ),
-          ),
-        ],
       ),
     );
   }
