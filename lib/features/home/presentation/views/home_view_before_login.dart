3b0cff194653e740a5c7b6e43b1fee0b06852b29
Javeed Ishaq <ishaqjaveed1@gmail.com>
2024-07-03 17:24:15 +0500

    welcome nft data integrated in home view before login

--- lib/features/home/presentation/views/home_view_before_login.dart
+++ lib/features/home/presentation/views/home_view_before_login.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile/app/core/enum/error_codes.dart';
@@ -13,7 +12,8 @@ import 'package:mobile/features/common/presentation/widgets/default_snackbar.dar
 import 'package:mobile/features/common/presentation/widgets/vertical_space.dart';
 import 'package:mobile/features/home/presentation/widgets/glassmorphic_button.dart';
 import 'package:mobile/features/home/presentation/widgets/nft_card_top_title_widget.dart';
-import 'package:mobile/features/home/presentation/widgets/nft_card_widget_parent_local.dart';
+import 'package:mobile/features/home/presentation/widgets/nft_card_widget_parent.dart';
+import 'package:mobile/features/nft/presentation/cubit/nft_cubit.dart';
 import 'package:mobile/features/wallets/presentation/cubit/wallets_cubit.dart';
 import 'package:mobile/generated/locale_keys.g.dart';
 import 'package:url_launcher/url_launcher_string.dart';
@@ -39,94 +39,113 @@ class HomeViewBeforeLogin extends StatelessWidget {
           "inside listener++++++ error message is $errorMessage".log();
         }
       },
-      child: Column(
-        children: [
-          const SizedBox(height: 50),
-          DefaultImage(
-            path: "assets/images/hide-me-please-logo.png",
-            width: 200,
-          ),
-          const SizedBox(height: 20),
-          Center(
-            child: Text(
-              "지갑을 연결하고\n웹컴 NFT를 받아보세요!",
-              textAlign: TextAlign.center,
-              style: fontR(18, lineHeight: 1.4),
-            ),
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all<Color>(bgNega4),
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(50.0),
-              )),
-              overlayColor:
-                  MaterialStateProperty.all<Color>(Colors.transparent),
-            ),
-            onPressed: onConnectWallet,
-            child: Text(
-              LocaleKeys.walletConnection.tr(),
-              style: fontCompactMdMedium(color: white),
-            ),
-          ),
-          const SizedBox(height: 30),
-          NFTCardWidgetParentLocal(
-            imagePath: "assets/images/home_card_img.png",
-            topWidget: const NftCardTopTitleWidget(
-              title: "Ready To Hide",
-              chain: "KLAYTN",
-            ),
-            badgeWidget: CustomImageView(
-              imagePath: "assets/images/free-graphic-text.png",
-            ),
-            bottomWidget: Padding(
-              padding: const EdgeInsets.only(left: 20.0, right: 20, bottom: 20),
-              child: Column(
+      child: BlocConsumer<NftCubit, NftState>(
+        bloc: getIt<NftCubit>(),
+        listener: (context, nftState) {},
+        builder: (context, nftState) {
+          return Column(
+            children: [
+              const SizedBox(height: 50),
+              DefaultImage(
+                path: "assets/images/hide-me-please-logo.png",
+                width: 200,
+              ),
+              const SizedBox(height: 20),
+              Center(
+                child: Text(
+                  "지갑을 연결하고\n웹컴 NFT를 받아보세요!",
+                  textAlign: TextAlign.center,
+                  style: fontR(18, lineHeight: 1.4),
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(bgNega4),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50.0),
+                  )),
+                  overlayColor:
+                      MaterialStateProperty.all<Color>(Colors.transparent),
+                ),
+                onPressed: onConnectWallet,
+                child: Text(
+                  LocaleKeys.walletConnection.tr(),
+                  style: fontCompactMdMedium(color: white),
+                ),
+              ),
+              const SizedBox(height: 5),
+              Stack(
                 children: [
-                  GlassmorphicButton(
-                    width: MediaQuery.of(context).size.width * 0.80,
-                    height: 60,
-                    onPressed: () {
-                      if (Platform.isAndroid) {
-                        launchUrlString(
-                            'https://play.google.com/store/apps/details?id=com.klipwallet.app');
-                      } else {
-                        launchUrlString(
-                            'https://apps.apple.com/app/id1627665524');
-                      }
-                    },
-                    child: Text(
-                      'Klip 설치',
-                      style: fontCompactLgMedium(),
+                  Container(
+                    margin: const EdgeInsets.only(top: 20),
+                    child: NFTCardWidgetParent(
+                      imagePath: nftState.welcomeNftEntity.image,
+                      topWidget: NftCardTopTitleWidget(
+                        title: nftState.welcomeNftEntity.name,
+                        chain: "KLAYTN",
+                      ),
+                      bottomWidget: Padding(
+                        padding: const EdgeInsets.only(
+                            left: 20.0, right: 20, bottom: 20),
+                        child: Column(
+                          children: [
+                            GlassmorphicButton(
+                              width: MediaQuery.of(context).size.width * 0.80,
+                              height: 60,
+                              onPressed: () {
+                                if (Platform.isAndroid) {
+                                  launchUrlString(
+                                      'https://play.google.com/store/apps/details?id=com.klipwallet.app');
+                                } else {
+                                  launchUrlString(
+                                      'https://apps.apple.com/app/id1627665524');
+                                }
+                              },
+                              child: Text(
+                                'Klip 설치',
+                                style: fontCompactLgMedium(),
+                              ),
+                            ),
+                            const VerticalSpace(12),
+                            GlassmorphicButton(
+                              width: MediaQuery.of(context).size.width * 0.80,
+                              height: 60,
+                              onPressed: onConnectWallet,
+                              child: Text(
+                                'Klip 연동',
+                                style: fontCompactLgMedium(),
+                              ),
+                            ),
+                            const VerticalSpace(12),
+                            GlassmorphicButton(
+                              width: MediaQuery.of(context).size.width * 0.80,
+                              height: 60,
+                              onPressed: onConnectWallet,
+                              child: Text(
+                                LocaleKeys.getNftForFree.tr(),
+                                style: fontCompactMdMedium(),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      index: 0,
                     ),
                   ),
-                  const VerticalSpace(12),
-                  GlassmorphicButton(
-                    width: MediaQuery.of(context).size.width * 0.80,
-                    height: 60,
-                    onPressed: onConnectWallet,
-                    child: Text(
-                      'Klip 연동',
-                      style: fontCompactLgMedium(),
-                    ),
-                  ),
-                  const VerticalSpace(12),
-                  GlassmorphicButton(
-                    width: MediaQuery.of(context).size.width * 0.80,
-                    height: 60,
-                    onPressed: onConnectWallet,
-                    child: Text(
-                      LocaleKeys.getNftForFree.tr(),
-                      style: fontCompactMdMedium(),
+                  Positioned(
+                    right: 0,
+                    top: 0,
+                    child: CustomImageView(
+                      imagePath: "assets/images/free-graphic-text.png",
                     ),
                   ),
                 ],
               ),
-            ),
-          )
-        ],
+            ],
+          );
+        },
       ),
     );
   }
